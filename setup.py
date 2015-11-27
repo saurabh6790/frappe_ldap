8a074298ad96f2ac76ca06ab1238970f58f103db
Saurabh <saurabh6790@gmail.com>
2015-11-27 14:40:55 +0530

    [fixes] pre-requisites fixes

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.0.1'
+version = '1.0.0'
+
+with open("requirements.txt", "r") as f:
+	install_requires = f.readlines()
 
 setup(
     name='frappe_ldap',
     version=version,
     description='Ldap Auth',
-    author='New Indictrnas',
-    author_email='saurabh.p@indinctranstech.com',
+    author='Frappe',
+    author_email='hello@frappe.io',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=("frappe",),
+    install_requires=install_requires,
 )
